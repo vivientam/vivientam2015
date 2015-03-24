b7ff5d58bf96de4f85bc6befde6c6cfc063d74bd
TamVivien <VivienTam@TamViviens-MacBook-Pro.local>
2015-03-24 16:23:27 +0800

    art page build

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150324063534) do
+ActiveRecord::Schema.define(version: 20150324081323) do
+
+  create_table "arts", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "designs", force: true do |t|
     t.datetime "created_at"
